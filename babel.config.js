661e94b80416a32c37d270f705b6f1c0d6ff5ae4
Kenny Yang <yangkennyk@gmail.com>
2020-06-20 14:32:14 -0700

    added babel transform plugins

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,12 @@
-module.exports = api => {
+module.exports = (api) => {
   api.cache.forever();
   return {
-    presets: [
-      "@babel/preset-env",
-      "@babel/preset-react"
-    ],
+    presets: ['@babel/preset-env', '@babel/preset-react'],
     plugins: [
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-proposal-object-rest-spread"
-    ]
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-transform-modules-commonjs',
+      'transform-es2015-modules-commonjs',
+    ],
   };
-}
+};
